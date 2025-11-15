e8785e367e3834e6ec4ac073bfb024d719e0230f
Aditya Patra <adityabpatra@gmail.com>
2025-11-14 18:02:23 -0800

    initialize with 2 car objects

--- main.cpp
+++ main.cpp
@@ -8,5 +8,9 @@ int main() {
     Car* temp;
     temp = new Car();
     carList.push_back(*temp);
-    carList[0].print();
+    temp = new Car();
+    carList.push_back(*temp);
+    for(int i = 0; i < 2; i++) {
+        carList[i].print();
+    }
 }
