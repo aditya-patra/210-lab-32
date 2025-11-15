f1e1017512d0ff62bb251f85cbb0562863a2570a
Aditya Patra <adityabpatra@gmail.com>
2025-11-14 18:07:30 -0800

    need to create new car object

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ const int START_SIZE = 2;
 int main() {
     deque<Car> carList;
     Car* temp;
-    temp = new Car();
     // add 2 Car objects to car line to start
     for (int i = 0; i < START_SIZE; i++) {
+        temp = new Car();
         carList.push_back(*temp);
     }
     for(int i = 0; i < 2; i++) {
